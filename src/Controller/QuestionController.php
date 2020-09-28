0ab19333752b2811787c4ae4c897390f57cc08a2
morganfraisse <morgan.fraisse@hotmail.fr>
2020-09-28 17:31:13 +0200

    chapitre 11 (suppr erreur)

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -29,7 +29,7 @@ class QuestionController extends AbstractController
             'un caca',
         ];
 
-        dump();
+        dump($answers);
 
 //         Retourne obligatoirement un objet Response
         return $this->render('question/show.html.twig', [
