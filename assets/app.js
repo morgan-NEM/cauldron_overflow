7a06eef61d7f2aec582dc3022a3585c8fa6038fc
morganfraisse <morgan.fraisse@hotmail.fr>
2020-10-05 16:13:14 +0200

    Chapitre 18 fin du cours

--- assets/app.js
+++ assets/app.js
@@ -9,6 +9,19 @@
 import './styles/app.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery'
 
 console.log('Hello Webpack Encore! Edit me in assets/app.js');
+let container = $(".js-vote-arrow-class")
+container.find('a').on('click', function (e){
+    e.preventDefault();
+
+    let link = $(e.currentTarget);
+    console.log('coucou')
+    $.ajax({
+        url: '/comments/10/vote/' + link.data('direction'),
+        method: 'POST'
+    }).then(function (data){
+        container.find('.js-vote-total').text(data.votes)
+    })
+})
