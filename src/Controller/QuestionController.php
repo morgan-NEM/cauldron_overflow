6791fbdf267f0935bd5ef443b5575e7516b40f8f
morganfraisse <morgan.fraisse@hotmail.fr>
2020-10-14 17:24:49 +0200

    Cours 2 chapitre 8

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -46,7 +46,7 @@ class QuestionController extends AbstractController
             return $markdownParser->transformMarkdown($questionText);
         });
 
-        dd($markdownParser);
+        dump($cache);
 
 //         Retourne obligatoirement un objet Response
         return $this->render('question/show.html.twig', [
