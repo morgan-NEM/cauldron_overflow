c812b3e6e2f4d0e6a7f4373c2c61f76f866bfe2b
morganfraisse <morgan.fraisse@hotmail.fr>
2020-10-14 14:51:45 +0200

    cours 2 chapitre 4

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,9 +4,11 @@
 namespace App\Controller;
 
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -29,18 +31,24 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{any}", name="app_question_show")
      */
-    public function show($any)
+    public function show($any, MarkdownParserInterface $markdownParser, CacheInterface $cache)
     {
         $answers = [
             'un marron',
             'Alors là...',
-            'un caca',
+            'un `caca`',
         ];
+        $questionText = "Qu'est-ce *qui* est **petit** et **marron**?";
+        $parsedQuestionTest = $cache->get('markdown_'.md5($questionText), function () use ($questionText, $markdownParser
+        ){
+            return $markdownParser->transformMarkdown($questionText);
+        });
 
 
 //         Retourne obligatoirement un objet Response
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ',  $any)),
+            'questionText' => $parsedQuestionTest,
             'answers' => $answers
         ]);
     }
