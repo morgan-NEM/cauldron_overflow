fac67aed3a5ef254e9db5cbb4253da2a8f4f2938
morganfraisse <morgan.fraisse@hotmail.fr>
2020-11-10 17:03:00 +0100

    Cours 3 chapitre 14

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,7 +4,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
 use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -27,9 +30,12 @@ class QuestionController extends AbstractController
     /**
      * @Route("/", name="app_accueil")
      */
-    public function homepage(Environment $twigEnvironment)
+    public function homepage(QuestionRepository $repository)
     {
-        return $this->render('question/homepage.html.twig');
+        $questions = $repository->findAllAskedOrderedByNewest();
+        return $this->render('question/homepage.html.twig', [
+            'questions' => $questions,
+        ]);
 
         /*
         Décorticage de la fonction render via les services Twig
@@ -40,23 +46,58 @@ class QuestionController extends AbstractController
     }
 
     /**
+     * @Route ("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager)
+    {
+        $question = new Question();
+        $question->setName('Missing pants')
+            ->setSlug('missing-pants-'.rand(0,1000))
+            ->setQuestion(<<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+Does anyone have a spell to call your pants back?
+EOF
+            );
+
+        if (rand(1,10)>2){
+            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1,100))));
+        }
+
+        $entityManager->persist($question);
+        $entityManager->flush();
+
+        return new Response(sprintf(
+            'Well hallo! The shiny new question is id #%d, slug: %s',
+            $question->getId(),
+        $question->getSlug()
+        ));
+    }
+
+    /**
      * @Route("/questions/{any}", name="app_question_show")
      */
-    public function show($any, MarkdownHelper $markdownHelper)
+    public function show($any, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
     {
+        $repository = $entityManager->getRepository(Question::class);
+        /** @var Question|null $question */
+        $question = $repository->findOneBy(['slug' => $any]);
+        if (!$question) {
+            throw $this->createNotFoundException(sprintf('no question found'));
+        }
         $answers = [
             'un marron',
             'Alors là...',
             'un `caca`',
         ];
-        $questionText = "Qu'est-ce *qui* est **petit** et **marron**?";
-
-        $parsedQuestionTest = $markdownHelper->parse($questionText);
 
 //         Retourne obligatoirement un objet Response
         return $this->render('question/show.html.twig', [
-            'question' => ucwords(str_replace('-', ' ',  $any)),
-            'questionText' => $parsedQuestionTest,
+            'question' => $question,
             'answers' => $answers
         ]);
 
